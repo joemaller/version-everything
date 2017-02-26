e86f3392478a06a453b898e4cfcfde5743988978
Joe Maller <joe@ideasonpurpose.com>
2017-02-26 07:29:31 -0500

    eslint dotfile

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    'no-param-reassign': 'error',
+    'space-before-function-paren': 'off',
+    'vars-on-top': 'off',
+    'no-console': 'warn',
+    'indent': ['error', 2 ],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single', { allowTemplateLiterals: true }],
+    'semi': ['error', 'always']
+  }
+};
