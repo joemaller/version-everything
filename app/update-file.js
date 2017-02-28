1219b0ebec5dabe245114c8fe73c9ac0a5cae27c
Joe Maller <joe@ideasonpurpose.com>
2017-02-28 14:59:31 -0500

    update-file json tests are all passing

--- app/update-file.js
+++ app/update-file.js
@@ -1,21 +1,34 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
+// const Promise = require("bluebird");
+// const readFile = Promise.promisify(fs.readFile);
+const chalk = require('chalk');
 const sed = require('shelljs').sed;
-const jsonfile = require('jsonfile');
-const yaml = require('js-yaml')
+const yaml = require('js-yaml');
 const xml2js = require('xml2js');
 const builder = new xml2js.Builder();
 
-const bumpPlainText = function(data) {
-
+/**
+ * Simple conditional wrapper for console.log
+ * Honest question. Did I just curry console.log?
+ * @param  {boolean} quiet  switch to enable logging
+ * @return {function}       console.log or a noop
+ */
+const logInit = function(quiet) {
+  return (quiet) ? ()=>{} : console.log;
 };
+
+const bumpPlainText = function(data) {
+  console.log(data);
+};
+
+
 
 const bumpJSON = function(data, version, config) {
   try {
-    let json = JSON.parse(data);
-    console.log(json);
+    let json = JSON.parse(data, config.reviver);
     const oldVersion = json.version;
     json.version = version;
     return {
@@ -23,50 +36,76 @@ const bumpJSON = function(data, version, config) {
       data: JSON.stringify(json, config.replacer, config.space)
     };
   } catch (err) {
-    console.log(err);
+    console.log('bumpJSON Error:', err);
+    throw err;
   }
-}
+};
 
 /**
  * Sends files to the correct bumping function, writes the result
  * @param  {[type]} file    [description]
  * @param  {[type]} version [description]
- * @return {[type]}         [description]
+ * @param {object} options Config options, quiet, json, xml and yaml
+ * @param {[function]} cb   standard node callback mostly used for testing
  */
-module.exports = function(file, version, options) {
+module.exports = function(file, version, options, cb) {
   if (!file) throw new Error('A file argument is required.');
   if (!version) throw new Error('A version argument (string) is required.');
   const defaults = {
-    json: {space: 2, replacer: null},
+    quiet: false,
+    json: {space: 2, replacer: null, reviver: null},
     xml: {},
     yaml: {}
   };
+
   const config = Object.assign({}, defaults, options);
-  // console.log(config);
+  const log = logInit(config.quiet);
 
   // first pass tries file extensions, if those don't match, try parsing
-  // the file contents  before falling back to plain-text regex replace
+  // the file contents before falling back to plain-text regex replace
   // console.log(path.extname(file).toLowerCase())
 
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) throw err;
-    let result;
+    let result = false;
 
     switch (path.extname(file).toLowerCase()) {
-      case ".json":
-        console.log('JSON FILE!', data);
-        result = bumpJSON(data, version, config.json);
-        console.log(result);
-        break;
-      case ".xml":
-      case ".plist":
-        console.log('XML or PLIST FILE!')
-        break;
-      default:
-        console.log('no extension match');
+    case '.json':
+      result = bumpJSON(data, version, config.json);
+      break;
+    case '.xml':
+    case '.plist':
+      // XML or PLIST FILE!
+      break;
+    case '.yml':
+    case '.yaml':
+      // YAML FILE!
+      break;
+    default:
+      // no extension match
+      // trying file as unmarked JSON
+      result = bumpJSON(data, version, config.json);
+      if (!result) {
+        // No result, trying file as XML
+      }
+      if (!result) {
+        // No result, trying file as YAML
+      }
+      if (!result) {
+        // No result, trying file as plain text (RegExp)
+      }
     }
     if (result) {
-      console.log(`Updating ${path.relative(file)} from ${result.oldVersion} to ${version}`);
+      fs.writeFile(file, result.data, (err) => {
+        // const cwdfix = process.cwd().replace(/\/private\/var\//, '/var/');
+        // const relFile = path.relative(cwdfix, file);
+        log(`Updated ${chalk.yellow(file)} from ${chalk.gray(result.oldVersion)} to ${chalk.green(version)}`);
+        cb(err, result);
+      });
+    } else {
+      if (cb && typeof cb === 'function') {
+        cb(err);
+      }
     }
-  })
+  });
 };
--- test/update-file.js
+++ test/update-file.js
@@ -1,44 +1,185 @@
+/* eslint-env node,mocha */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "should" }]*/
+
 'use strict';
-const path = require('path');
+
 const should = require('chai').should();
 const assert = require('chai').assert;
+
+const fs = require('fs-extra');
+const tmp = require('tmp');
+const semver = require('semver');
+
+tmp.setGracefulCleanup();
+
 const updateFile = require('../app/update-file');
 
+
+
+
 describe('Update a file', function() {
+  let newVersion = '1.0.0';
+  let tmpDir;
+  const cwd = process.cwd();
+  const fixtureDir = './test/fixture/deep/dotfile';
+
+  beforeEach('refresh fixtures to tempfile', function() {
+    newVersion = semver.inc(newVersion, 'patch');
+    tmpDir = tmp.dirSync({unsafeCleanup: true}).name;
+    fs.copySync(fixtureDir, tmpDir);
+    process.chdir(tmpDir);
+  });
+
+
+  afterEach('clean up tmpDir', function() {
+    process.chdir(cwd);
+  });
+
 
   it('requires a file argument', function() {
     const file = function() {
       updateFile();
-    }
+    };
     file.should.throw(Error);
   });
+
 
   it('requires a version string argument', function() {
     const file = function() {
       updateFile('foo.file');
-    }
+    };
     file.should.throw(Error);
   });
 
-  // it('accepts a string file path', function() {
-  //   const foo = 5;
-  // })
-  // it('accepts a filestream', function() { assert.fail() })
-  it('should increment a plain text file (regex)', function() { assert.fail() })
-  it('should increment a json file', function() {
-    const file = updateFile('./test/fixture/deep/dotfile/file.json', '1.0.0');
-    file.should.have.string('foo');
+
+  it('accepts a string file path');
+  it('accepts a filestream');
+  it('should increment a plain text file (regex)');
+
+
+  it('should increment a json file', function(done) {
+    const file = 'file.json';
+    updateFile(file, newVersion, {quiet: true}, (err) => {
+      assert.ifError(err);
+      fs.readJson(file, (err, json) => {
+        assert.ifError(err);
+        json.should.have.property('version', newVersion);
+        done();
+      });
+    });
   });
-  it('should increment a json file with a replacer function', function() { assert.fail() })
-  it('should increment a json file with different indentation', function() { assert.fail() })
-  it('should increment a yaml file', function() { assert.fail() })
-  it('should increment an xml file', function() { assert.fail() })
-  it('should increment an xml plist file', function() { assert.fail() })
-  it('should increment a json file without a file extension', function() { assert.fail() })
-  it('should increment a yaml file without a file extension', function() { assert.fail() })
-  it('should increment an xml file without a file extension', function() { assert.fail() })
-  it('adds version to version-less json file', function() { assert.fail() })
-  it('passes version-less plain files through unchanged', function() { assert.fail() })
+
+
+  it('should increment a json file, also using a replacer array', function(done) {
+    const file = 'file.json';
+    const replacer = ['title', 'version'];
+    updateFile(file, newVersion, {json: {replacer: replacer}}, (err) => {
+      assert.ifError(err);
+      fs.readJson(file, (err, json) => {
+        assert.ifError(err);
+        json.should.have.property('version', newVersion);
+        json.should.have.property('title');
+        json.should.not.have.property('double_me');
+        done();
+      });
+    });
+  });
+
+
+  it('should increment a json file, also using a replacer function', function(done) {
+    const file = 'file.json';
+    const replacer = (key, value) => (key === 'double_me') ? value * 2 : value;
+    updateFile(file, newVersion, {json: {replacer: replacer}}, (err) => {
+      assert.ifError(err);
+      fs.readJson(file, (err, json) => {
+        json.should.have.property('version', newVersion);
+        json.should.have.property('title');
+        json.should.have.property('double_me', 10);
+        done();
+      })
+    });
+  });
+
+
+  it('should increment a json file, also using a reviver function', function(done) {
+    const file = 'file.json';
+    const reviver = (key, value) => (key === 'double_me') ? value * 2 : value;
+    updateFile(file, newVersion, {json: {reviver: reviver}}, (err, result) => {
+      assert.ifError(err);
+      fs.readJson(file, (err, json) => {
+        json.should.have.property('version', newVersion);
+        json.should.have.property('title');
+        json.should.have.property('double_me', 10);
+        done();
+      })
+    });
+  });
+
+
+  it('should increment a json file and set a specific indentation', function(done) {
+    const file = 'file.json';
+    const spaces = 4;
+    const regex = new RegExp('^ {' + spaces + '}"version":', 'm');
+    updateFile(file, newVersion, {json:{space: spaces}}, (err, result) => {
+      assert.ifError(err);
+      fs.readFile(file, (err, data) => {
+        data.toString().should.match(regex);
+        done();
+      });
+    });
+  });
+
+
+  it('should increment a yaml file');
+  // it('should increment a yaml file', function(done) {
+  //   const file = 'file.yml';
+  //   const regex = new RegExp('^version: ["\']' + newVersion, 'm');
+  //   console.log('*&***************&*&*&*&*&*&    ' + regex );
+  //   updateFile(file, newVersion, {}, (err, result) => {
+  //     assert.ifError(err);
+  //     fs.readFile(file, (err, data) => {
+  //       // console.log(data.toString());
+  //       data.toString().should.match(regex);
+  //       // data.toString().should.match(/^     "version"/m);
+  //       // assert.fail();
+  //       done();
+  //     });
+  //   });
+  // });
+
+
+  it('should increment an xml file');
+  it('should increment an xml plist file');
+
+
+  it('should increment a json file without a file extension', function(done) {
+    const file = 'naked-json';
+    updateFile(file, newVersion, {}, (err) => {
+      assert.ifError(err);
+      fs.readJson(file, (err, json) => {
+        json.should.have.property('version', newVersion);
+        done();
+      })
+    });
+  });
+
+
+  it('should increment an xml file without a file extension');
+  it('should increment a yaml file without a file extension');
+
+
+  it('adds version to version-less json file', function(done) {
+    const file = 'no-version.json';
+    updateFile(file, newVersion, {}, (err) => {
+      assert.ifError(err);
+      fs.readJson(file, (err, json) => {
+        json.should.have.property('version', newVersion);
+        done();
+      });
+    });
+  });
+  it('passes version-less plain files through unchanged');
+
 
   describe('Test output (console.log)', function() {
     let output;
@@ -60,18 +201,49 @@ describe('Update a file', function() {
 
     afterEach(cleanup);
 
-    it('shows the file, current version and updated version', function() {
+    it('should be quiet', function(done) {
+      const file = 'file.json';
       try {
-        const file = '.path/to/file.js'
-        const updatedFile = updateFile(file, '1.0.0');
-        output.should.have.string(file)
-        output.should.match(/updating somefile from 8.0.1 to 1.2.23/);
-        cleanup();
+        updateFile(file, newVersion, {quiet: true}, (err) => {
+          assert.ifError(err);
+          output.should.be.empty;
+          done();
+        });
       } catch (err) {
-        cleanup();
-        throw err
+        assert.ifError(err);
       }
+      cleanup();
     });
 
-  })
+
+    it('should be loud', function(done) {
+      const file = 'file.json';
+      try {
+        updateFile(file, newVersion, {}, (err) => {
+          assert.ifError(err);
+          output.should.not.be.empty;
+          done();
+        });
+      } catch (err) {
+        assert.ifError(err);
+      }
+      cleanup();
+    });
+
+
+    it('shows the file, current version and updated version', function(done) {
+      const file = 'file.json';
+      try {
+        updateFile(file, newVersion, {}, (err, result) => {
+          output.should.have.string(file);
+          output.should.have.string(newVersion);
+          output.should.have.string(result.oldVersion);
+          done();
+        });
+      } catch (err) {
+        assert.ifError(err);
+      }
+      cleanup();
+    });
+  });
 });
