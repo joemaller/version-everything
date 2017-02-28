660fb4c6acae4c4888d95ed010ddb71763aeebc8
Joe Maller <joe@ideasonpurpose.com>
2017-02-28 15:06:14 -0500

    move bumpJSON into its own file

---
+++ app/lib/bumpJSON.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(data, version, config) {
+  try {
+    let json = JSON.parse(data, config.reviver);
+    const oldVersion = json.version;
+    json.version = version;
+    return {
+      oldVersion: oldVersion,
+      data: JSON.stringify(json, config.replacer, config.space)
+    };
+  } catch (err) {
+    console.log('bumpJSON Error:', err);
+    throw err;
+  }
+};
--- app/update-file.js
+++ app/update-file.js
@@ -25,21 +25,7 @@ const bumpPlainText = function(data) {
 };
 
 
-
-const bumpJSON = function(data, version, config) {
-  try {
-    let json = JSON.parse(data, config.reviver);
-    const oldVersion = json.version;
-    json.version = version;
-    return {
-      oldVersion: oldVersion,
-      data: JSON.stringify(json, config.replacer, config.space)
-    };
-  } catch (err) {
-    console.log('bumpJSON Error:', err);
-    throw err;
-  }
-};
+const bumpJSON = require('./lib/bumpJSON');
 
 /**
  * Sends files to the correct bumping function, writes the result
