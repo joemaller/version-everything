59e9df3a4eb0eae38fb4c189f7fc991c3f54679c
Joe Maller <joe@ideasonpurpose.com>
2017-02-28 14:48:53 -0500

    switch from jsonfile to fs-extra.readJson

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const sed = require('shelljs').sed;
-const jsonfile = require('jsonfile');
-const yaml = require('js-yaml')
+const yaml = require('js-yaml');
 const xml2js = require('xml2js');
 const builder = new xml2js.Builder();
 
@@ -26,14 +25,14 @@ module.exports = function(config) {
   console.log('------------------ in main function ---------------');
   console.log('process.argv', process.argv);
 
-  const pkg = getPackageJson(config)
+  const pkg = getPackageJson(config);
   const files = getVersionFiles(config, pkg);
 
   console.log(pkg, files);
   return;
 
   files.forEach(f => {
-    jsonfile.readFile(f, (err, data) => {
+    fs.readJson(f, (err, data) => {
       if (err) {
        sed('-i', /^([# ]*Version: *).*/, `$1${ pkg.version }`, f);
       } else  {
