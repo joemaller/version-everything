06908a75c3d20c4e36e9fa3cd3f9f0e20da73b62
Joe Maller <joe@joemaller.com>
2022-01-10 18:08:33 -0500

    refactor bumpyaml

--- app/lib/bump-yaml.js
+++ app/lib/bump-yaml.js
@@ -14,33 +14,28 @@ module.exports = function (data, version, config) {
   let prefixData = {};
   let hasVersion = false;
 
-  try {
-    if (config.prefixes.length > 0) {
-      prefixData = bumpPlainText(data, version, config);
-      hasVersion = !!prefixData?.data;
-    }
-
-    /** @type {object} */
-    const yamlObj = YAML.parse(prefixData.data || data);
-
-    // TODO: Does this also need to check for Arrays and Strings?
-    if (yamlObj === null) {
-      throw "YAML.parse() returned null";
-    }
-
-    const yamlData = YAML.parseDocument(prefixData?.data || data, config.yaml);
-    const oldVersion = yamlData.get("version");
-
-    if (!hasVersion) {
-      yamlData.set("version", version);
-    }
-
-    return {
-      oldVersion: oldVersion,
-      data: yamlData.toString(config.yaml),
-    };
-  } catch (err) {
-    // console.log("bumpYAML Error:", err, data);
-    throw err;
+  if (config.prefixes.length > 0) {
+    prefixData = bumpPlainText(data, version, config);
+    hasVersion = !!prefixData?.data;
   }
+
+  /** @type {object} */
+  const yamlObj = YAML.parse(prefixData.data || data);
+
+  // TODO: Does this also need to check for Arrays and Strings?
+  if (yamlObj === null) {
+    throw "YAML.parse() returned null";
+  }
+
+  const yamlData = YAML.parseDocument(prefixData?.data || data, config.yaml);
+  const oldVersion = yamlData.get("version");
+
+  if (!hasVersion) {
+    yamlData.set("version", version);
+  }
+
+  return {
+    oldVersion: oldVersion,
+    data: yamlData.toString(config.yaml),
+  };
 };
